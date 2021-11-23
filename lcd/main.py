8079ac508820f358c0082c7656c442fcbc72b32d
avidadearthur <arthurtquintao@gmail.com>
2021-11-23 16:46:55 +0100

    Start thread from get_sentence thread

--- lcd/main.py
+++ lcd/main.py
@@ -7,12 +7,27 @@ import lcddriver
 import threading
 
 
+# worker thread - displays the sentence on screen
 def worker(sentence):
     # Do some stuff
     print("Thread started successfully ")
+    print([th.name for th in threading.enumerate()])
     lcd.lcd_display_string(sentence, 1)
     print("Closing thread... ")
     sleep(3)
+
+
+# input thread, asks for sentence without stopping clock
+def get_sentence():
+    sentence = input("Enter a sentence: ")
+    print("Starting thread...")
+
+    lcd.lcd_clear()
+
+    t = threading.Thread(target=worker, args=(sentence,), name="t")  # Always put a comma after the arguments. Even if you have only one arg.
+    t.start()  # Start the thread
+    t.join()  # Join main thread to avoid competition over display
+
 
 
 def run_cmd(cmd):
@@ -41,17 +56,15 @@ if __name__ == "__main__":
 
         # thread checking
         if "t" not in [th.name for th in threading.enumerate()]:
-            sentence = input("Enter a sentence: ")
-            print("Starting thread...")
-            lcd.lcd_clear()
-            t = threading.Thread(target=worker, args=(sentence,), name="t")  # Always put a comma after the arguments. Even if you have only one arg.
-            t.start()  # Start the thread
-            t.join()  # Join main thread to avoid competition over display
 
-        if sentence == "stop":
-                print("Waiting for the function to finish...")
-                t.join()  # Stop the thread (NOTE: the program will wait for the function to finish)
-                break
+            s = threading.Thread(target=worker, name="s")
+            s.start()
+
+        # if sentence == "stop":
+        #         print("Waiting for the function to finish...")
+        #         t.join()  # Stop the thread (NOTE: the program will wait for the function to finish)
+        #         s.join()
+        #         break
         else:
             print("Thread closed")
 
