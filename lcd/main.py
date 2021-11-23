eb8cdb8fd1921569e91724406ee2e00b7f5bda7e
avidadearthur <arthurtquintao@gmail.com>
2021-11-23 17:19:01 +0100

    Revert "Add a new thread and a global variable to hold the sentence"
    This reverts commit 477fb67c535e2f24e77c8230c40140a0c236b14f.

--- lcd/main.py
+++ lcd/main.py
@@ -6,23 +6,14 @@ from subprocess import *
 import lcddriver
 import threading
 
-global sentence  # dummy variable
-sentence = ''
 
-
-# displays sentence
-def worker():
+def worker(sentence):
     # Do some stuff
-    print("t thread started successfully ")
+    print("Thread started successfully ")
     print([th.name for th in threading.enumerate()])
     lcd.lcd_display_string(sentence, 1)
-    print("Closing t thread... ")
+    print("Closing thread... ")
     sleep(3)
-
-
-# changes global variable sentence using input
-def get_sentence():
-    sentence = input("Enter a sentence: ")
 
 
 def run_cmd(cmd):
@@ -51,23 +42,18 @@ if __name__ == "__main__":
 
         # thread checking
         if "t" not in [th.name for th in threading.enumerate()]:
-
-            print("Starting s thread...")
-            s = threading.Thread(target=get_sentence, name="s")
-            s.start()
-
-            print("Starting t thread...")
+            sentence = input("Enter a sentence: ")
+            print("Starting thread...")
             lcd.lcd_clear()
-            t = threading.Thread(target=worker, name="t")  # Always put a comma after the arguments. Even if you have only one arg.
+            t = threading.Thread(target=worker, args=(sentence,), name="t")  # Always put a comma after the arguments. Even if you have only one arg.
             t.start()  # Start the thread
             t.join()  # Join main thread to avoid competition over display
 
         if sentence == "stop":
-            print("Waiting for all functions to finish...")
-            t.join()  # Stop the thread (NOTE: the program will wait for the function to finish)
-            break
+                print("Waiting for the function to finish...")
+                t.join()  # Stop the thread (NOTE: the program will wait for the function to finish)
+                break
         else:
             print("Thread closed")
-            print([th.name for th in threading.enumerate()])
 
 
