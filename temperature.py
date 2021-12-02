ea6c61a3168e637d1c142f269cff3e2dfef4a50f
avidadearthur <arthurtquintao@gmail.com>
2021-12-03 00:52:50 +0100

    new temperature test code

--- temperature.py
+++ temperature.py
@@ -1,31 +1,15 @@
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008
-import spidev
-import math
+## PINOUT BCM ##
+#CLK = 23
+#MISO = 21
+#MOSI = 19
+#CS = 24
+
+from gpiozero import MCP3008
 from time import sleep
 
-## PINOUT BCM ##
-CLK = 23
-MISO = 21
-MOSI = 19
-CS = 24
+ldr = MCP3008(channel=0, 
+    clock_pin=23, mosi_pin=19, miso_pin=21, select_pin=24)
 
-
-tempChannel = 0
-sleepTime = 1
-
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS,miso=MISO,mosi=MOSI) 
-#mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(1,0))
-def mcpRead(channel):
-    return mcp.read_adc(channel)
-
-
-while (1):
-    adc = mcpRead(tempChannel)
-    print(adc)
-    inBeta = 1.0/3950.0
-    inTO = 1.0/298.15
-    k = 1.0/(inTO+inBeta*math.log(1023/adc-1.0))
-    temperature = k-273.15
-    print("Temp: %s"%(temperature))
-    sleep(sleepTime)
+while True:
+    print(ldr.value)
+    sleep(0.5)
