44b3e460864b128fab3c5cad31bbb6f6b12f0546
avidadearthur <arthurtquintao@gmail.com>
2021-12-03 01:10:12 +0100

    Revert "change pins"
    This reverts commit 194640cf45408be3d4b41f7af068805cee8b966b.

--- temperature.py
+++ temperature.py
@@ -1,8 +1,14 @@
+## PINOUT BCM ##
+#CLK = 23
+#MISO = 21
+#MOSI = 19
+#CS = 24
+
 from gpiozero import MCP3008
 from time import sleep
 
 ldr = MCP3008(channel=0, 
-clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=8)
+    clock_pin=23, mosi_pin=19, miso_pin=21, select_pin=29)
 
 while True:
     print(ldr.value)
