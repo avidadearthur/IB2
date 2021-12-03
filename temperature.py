b7e299f8c680cf39c29c795c271ff0d6c66ae36e
avidadearthur <arthurtquintao@gmail.com>
2021-12-03 01:16:09 +0100

    trying to fix pins

--- temperature.py
+++ temperature.py
@@ -2,7 +2,7 @@ from gpiozero import MCP3008
 from time import sleep
 
 ldr = MCP3008(channel=0, 
-    clock_pin=23, mosi_pin=19, miso_pin=21, select_pin=29)
+    clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=5)
 
 while True:
     print(ldr.value)
