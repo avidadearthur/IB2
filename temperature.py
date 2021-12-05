e119b3a6b1726017167f5f89c0dcd88fab1a1daf
avidadearthur <arthurtquintao@gmail.com>
2021-12-05 23:10:51 +0100

    Test after enambling SPI

--- temperature.py
+++ temperature.py
@@ -1,8 +1,7 @@
 from gpiozero import MCP3008
 from time import sleep
 
-ldr = MCP3008(channel=0, 
-    clock_pin=23, mosi_pin=19, miso_pin=21)
+ldr = MCP3008(channel=0)
 
 while True:
     print(ldr.value)
