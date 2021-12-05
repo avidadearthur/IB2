8d64cc5b27eb66e0f2eb865c2dbe9e76a4c6b60c
avidadearthur <arthurtquintao@gmail.com>
2021-12-05 23:46:13 +0100

    Update alarm file for testing

--- lcd/alarm.py
+++ lcd/alarm.py
@@ -1,14 +1,19 @@
-#import modules
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from time import sleep, strftime, time
+import lcddriver
 
-class alarm:
-    #initializes the alarm object
-    def __init__(self):
-    sleep(0.2)
 
-    #gets the current alarm time
-    def get_time(self):
-        return
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+# Set pins to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # SET
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # UP
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # DOWN
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # RESET
 
-    #set alarm time
-    def set_time(self):
-        return
+lcd = lcddriver.lcd()
+
+while True:
+    lcd.lcd_display_string(strftime('TIME: ' '%I:%M:%S %p'), 1)
+    lcd.lcd_display_string(strftime('%a, %b %d %Y'), 2)
+    sleep(1)
