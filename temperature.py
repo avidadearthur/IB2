1447219928b8655cf3f6089b77af9b4e27b57c23
avidadearthur <arthurtquintao@gmail.com>
2021-12-07 14:32:09 +0100

    Fix temperature conversion formulas

--- temperature.py
+++ temperature.py
@@ -5,15 +5,28 @@ import math
 ldr = MCP3008(channel=0)
 
 while True:
+
+    ''' 
+    Resitor - 9.6k
+    NTC     - 3.36k @25C
+
+    T2 = 1/(1/T1 + ln(Rt/R)/B)
+
+    Rt - is the thermistor’s resistance under T2 temperature
+
+    R  - is the nominal resistance under T1 temperature - 9.6k 
     
+    B is the thermal index - 2880k
+    
+    T1, T2 is the temperature in Kelvin
+    We’ll use 25 for T1
+    '''
     value = ldr.value
     print(value)
 
-    voltage = value / 255.0 * 3.3
-    Rt = 2.7 * voltage / (3.3 - voltage)
-    tempK = 1/(1/(273.15 + 25) + math.log(Rt/9.6)/3950.0) 
+    voltage = ldr.value * 3.3
+    Rt = ldr.value*3.36
+    tempK = 1 / (1/(273.15 + 25) + math.log(Rt/9.6)/2880)
+    
     tempC = tempK -273.15
-    print ('ADC Value : %d, Voltage : %.2f, Temperature : %.2f'%(value,voltage,tempC))
-    sleep(0.01)
-
-    sleep(0.5)
+    print ('NTC resistance : %d k, Voltage : %.2f, Temperature : %.2f'%(Rt,voltage,tempC))
