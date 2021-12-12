b4d185f637befe2b22430502c3a5fa548687f205
avidadearthur <arthurtquintao@gmail.com>
2021-12-12 10:29:00 +0100

    change main code to fsm structure

--- lcd/main.py
+++ lcd/main.py
@@ -2,74 +2,57 @@
 # Date And Time Script
 
 from time import sleep, strftime, time
+
+from gpiozero import exc
 import lcddriver
 import threading
 import RPi.GPIO as GPIO
 
-def display_sentence(input_sentence):
-    # Do some stuff
-    print("Thread started successfully ")
-    lcd.lcd_display_string(input_sentence, 1)
-    print("Closing thread... ")
-    sleep(3)
-
-
-def clock():
-    # to be used during testing
-    t_end = time() + 30 # add timer for clock loop
-
-    while True:
-        # date & time display
-        if "sentence" not in [th.name for th in threading.enumerate()]:
-            lcd.lcd_display_string(strftime('TIME: ' '%I:%M:%S %p'), 1)
-            lcd.lcd_display_string(strftime('%a, %b %d %Y'), 2)
-            sleep(1)
-        if off:
-            break
-        # avoid infinite loop due to errors
-        if time() > t_end:
-            break
-
 
 if __name__ == "__main__":
-    # if you call this script from the command line (the shell) it will
-    # run the 'main' function
-
+    
     GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-    GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 8 to be an input pin and set initial value to be pulled low (off)
+    # Set pins to be an input pin and set initial value to be pulled low (off)
+    GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # SET   GPIO23
+    GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # UP    GPIO22
+    GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # DOWN  GPIO27
+    GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # RESET GPIO17
 
     lcd = lcddriver.lcd()
 
-    global off
-    off = False
-    clock_thread = threading.Thread(target=clock, name="clock")
-    clock_thread.start()
+    # Possible states: 
+    # 0 - Clock Date & Time
+    # 1 - Sensors Data
+    # 2 - Alarm Set/Alarm Display
 
-    while True:
+    try:
+        curr_state = 0 # Set 0 as default state
+        while True:
+            # Use UP and DOWN GPIOs to move between states
+            # Arrow UP
+            if GPIO.input(15) == GPIO.HIGH:
+                next_state = curr_state + 1
 
-        #print([th.name for th in threading.enumerate()])
+            # Arrow DOWN
+            if GPIO.input(13) == GPIO.HIGH:
+                next_state = curr_state - 1
 
-        # It might be a good idea to block the GPIO once it is pressed
-        # to avoid bugs
-        if GPIO.input(10) == GPIO.HIGH:
+            # 0 - Clock Date & Time
+            if abs(curr_state) == 0:
+                print("Clock date & time")
+            
+            # 1 - Sensors Data
+            elif abs(curr_state) == 1:
+                print("Sensors Data")
+            
+            # 2 - Alarm Set/Alarm Display
+            elif abs(curr_state) == 2:
+                print("Alarm")
+                next_state = 0 # Go back to init state 
 
-            # thread checking
-            if "sentence" not in [th.name for th in threading.enumerate()]:
-                sentence = "GPIO 10 was set to high"
-                print("Starting thread...")
+    except KeyboardInterrupt:
+        lcd.lcd_clear()
 
-                lcd.lcd_clear()
-                # Always put a comma after the arguments.
-                # Even if you have only one arg.
-                sentence_display = threading.Thread(target=display_sentence, args=(sentence,), name="sentence")
-                sentence_display.start()  # Start the thread
-                sentence_display.join()  # Join main thread to avoid competition over display
-
-        if GPIO.input(8) == GPIO.HIGH:
-            print("Waiting for the function to finish...")
-            #sentence_display.join()  # Stop the thread (NOTE: the program will wait for the function to finish)
-            # Break the clock thread
-            off = True
-            break
+    
+    
