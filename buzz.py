56654cfb9bb03aedfa97331aa36dc8228dc3b380
avidadearthur <arthurtquintao@gmail.com>
2021-12-14 17:04:58 +0100

    Change buzzer code

--- buzz.py
+++ buzz.py
@@ -1,9 +1,18 @@
-import gpiozero
-
-out = gpiozero.PWMOutputDevice(12)
-
-
+#Libraries
+import RPi.GPIO as GPIO
+from time import sleep
+#Disable warnings (optional)
+GPIO.setwarnings(False)
+#Select GPIO mode
+GPIO.setmode(GPIO.BCM)
+#Set buzzer - pin 23 as output
+buzzer=12 
+GPIO.setup(buzzer,GPIO.OUT)
+#Run forever loop
 while True:
-    out.frequency = 10000
-    out.value = 0.5
-    #ss
+    GPIO.output(buzzer,GPIO.HIGH)
+    print ("Beep")
+    sleep(0.5) # Delay in seconds
+    GPIO.output(buzzer,GPIO.LOW)
+    print ("No Beep")
+    sleep(0.5)
