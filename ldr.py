d4e08c534d6671f086896276a8c6f483e6b02d98
avidadearthur <arthurtquintao@gmail.com>
2022-02-17 16:56:12 +0100

    Add conversion formula

--- ldr.py
+++ ldr.py
@@ -1,6 +1,7 @@
 import spidev
 import time
 import os
+from numpy import log as ln
  
 # Open SPI bus
 spi = spidev.SpiDev()
@@ -28,11 +29,18 @@ def ConvertTemp(voltz_out,places):
  
   R_o = 9660 
   voltz_src = 3.3
+
+  R_ref = 5000
+  A = 3.35402e-03
+  B = 2.51094e-04
+  C = 3.51094e-04
+  D = 1.10518e-07
  
-  R_t = ( R_o * voltz_out ) / ( voltz_src - voltz_out )
- 
-  temp = R_t
+  R_t = (R_o * voltz_out)/(voltz_src-voltz_out)
+
+  temp = (A + B*ln(R_t/R_ref) + C*(ln(R_t/R_ref)**2) + D*(ln(R_t/R_ref)**3))**(-1)
   temp = round(temp,places)
+  
   return temp
  
 # Define sensor channels
