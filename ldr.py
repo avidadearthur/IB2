89f624aef49781445af0453d6123877cbb685fab
avidadearthur <arthurtquintao@gmail.com>
2022-02-17 16:10:42 +0100

    Try to read resistance change

--- ldr.py
+++ ldr.py
@@ -22,22 +22,16 @@ def ConvertVolts(data,places):
   return volts
  
 # Function to calculate temperature from
-# TMP36 data, rounded to specified
+# NTC data, rounded to specified
 # number of decimal places.
-def ConvertTemp(data,places):
+def ConvertTemp(voltz_out,places):
  
-  # ADC Value
-  # (approx)  Temp  Volts
-  #    0      -50    0.00
-  #   78      -25    0.25
-  #  155        0    0.50
-  #  233       25    0.75
-  #  310       50    1.00
-  #  465      100    1.50
-  #  775      200    2.50
-  # 1023      280    3.30
+  R_o = 9680 
+  voltz_src = 3.3
  
-  temp = ((data * 330)/float(1023))-50
+  R_t = R_o * (( voltz_src / voltz_out ) - 1)
+ 
+  temp = R_t
   temp = round(temp,places)
   return temp
  
@@ -57,12 +51,12 @@ while True:
   # Read the temperature sensor data
   temp_level = ReadChannel(temp_channel)
   temp_volts = ConvertVolts(temp_level,2)
-  temp       = ConvertTemp(temp_level,2)
+  temp       = ConvertTemp(temp_volts,2)
  
   # Print out results
   print "--------------------------------------------"
   print("Light: {} ({}V)".format(light_level,light_volts))
-  print("Temp : {} ({}V) {} deg C".format(temp_level,temp_volts,temp))
+  print("Temp : {} ({}V) {} Ohms".format(temp_level,temp_volts,temp))
  
   # Wait before repeating loop
   time.sleep(delay)
