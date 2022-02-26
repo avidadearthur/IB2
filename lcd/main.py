32bcb1b4bda3802c6e284570ed7f2bc57d86e374
arthur <arthurtquintao@gmail.com>
2022-02-26 13:54:17 +0100

    Change hour field to 24h format

--- lcd/main.py
+++ lcd/main.py
@@ -167,7 +167,7 @@ def display_clock():
         if "alarm" not in [th.name for th in threading.enumerate()]:
             if "sensors" not in [th.name for th in threading.enumerate()]:
                 # Date & Time display
-                lcd.lcd_display_string(strftime('TIME: ' '%I:%M:%S'), 1)
+                lcd.lcd_display_string(strftime('TIME: ' '%H:%M:%S'), 1)
                 lcd.lcd_display_string(strftime('%a, %b %d %Y'), 2)
 
         # Stop displaying
