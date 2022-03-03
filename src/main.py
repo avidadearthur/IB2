28c902802af086a95157a930d65bc6c45a71f7b2
arthur <arthurtquintao@gmail.com>
2022-03-03 16:01:19 +0100

    Test date parsing

--- src/main.py
+++ src/main.py
@@ -71,8 +71,8 @@ def display_alarm():
                 else:
                     a = str(datetime_alarm[0][0])
                     print(a)
-                    print(a[:4], a[:4], a[5:7], a[8:11], a[11:])
-                    alarm_datetime = datetime.now() + timedelta(minutes=5)
+                    print(a[:4], a[:4], a[5:7], a[8:10], a[11:])
+                    alarm_datetime = datetime(int(a[:4]), int(a[:4]), int(a[5:7]), int(a[8:10]), int(a[11:]))
                     new_alarm = alarm_datetime
 
                     lcd.lcd_display_string('Nxt Alarm: {}'.format(new_alarm.strftime('%H:%M')), 1)
