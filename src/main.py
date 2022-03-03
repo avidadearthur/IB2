ed3132772d2b622602d32744ea73f703d1c6c40f
arthur <arthurtquintao@gmail.com>
2022-03-03 16:08:12 +0100

    Fix typo

--- src/main.py
+++ src/main.py
@@ -71,8 +71,8 @@ def display_alarm():
                 else:
                     a = str(datetime_alarm[0][0])
                     print(a)
-                    print(a[:4], a[:4], a[5:7], a[8:10], a[11:13], a[14:])
-                    alarm_datetime = datetime(int(a[:4]), int(a[:4]), int(a[5:7]), int(a[8:10]), int(a[11:13]), int(a[14:]))
+                    print(a[:4], a[5:7], a[8:10], a[11:13], a[14:])
+                    alarm_datetime = datetime(int(a[:4]), int(a[5:7]), int(a[8:10]), int(a[11:13]), int(a[14:]))
                     new_alarm = alarm_datetime
 
                     lcd.lcd_display_string('Nxt Alarm: {}'.format(new_alarm.strftime('%H:%M')), 1)
