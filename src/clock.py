9667738eeab2b0bc5dda03cffbb80376a193b797
arthur <arthurtquintao@gmail.com>
2022-03-09 15:48:34 +0100

    Try to move display clock function to another file

---
+++ src/clock.py
@@ -0,0 +1,32 @@
+import threading
+import RPi.GPIO as GPIO
+from time import strftime
+from src.lcddriver import lcd
+
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # RESET GPIO17
+
+
+def display_clock(clock_thread=None):
+    # Always start by clearing the LCD
+    lcd.lcd_clear()
+
+    while True:
+
+        if "alarm" not in [th.name for th in threading.enumerate()]:
+            if "sensors" not in [th.name for th in threading.enumerate()]:
+                # Date & Time display
+                lcd.lcd_display_string(strftime('TIME: ' '%H:%M:%S'), 1)
+                lcd.lcd_display_string(strftime('%a, %b %d %Y'), 2)
+
+        # Stop displaying
+        if GPIO.input(15) == GPIO.HIGH or GPIO.input(13) == GPIO.HIGH:
+            try:
+                clock_thread.join()
+            except RuntimeError:
+                break
+
+            break
+
+        # RESET button
+        if GPIO.input(11) == GPIO.HIGH:
+            break
--- src/main.py
+++ src/main.py
@@ -8,8 +8,9 @@ import lcddriver
 import sensors
 import threading
 import requests
-import sqlite3
 import RPi.GPIO as GPIO
+
+from src.clock import display_clock
 
 
 def display_alarm():
@@ -185,30 +186,7 @@ def display_sensors():
             break
 
 
-def display_clock():
-    # Always start by clearing the LCD
-    lcd.lcd_clear()
 
-    while True:
-
-        if "alarm" not in [th.name for th in threading.enumerate()]:
-            if "sensors" not in [th.name for th in threading.enumerate()]:
-                # Date & Time display
-                lcd.lcd_display_string(strftime('TIME: ' '%H:%M:%S'), 1)
-                lcd.lcd_display_string(strftime('%a, %b %d %Y'), 2)
-
-        # Stop displaying
-        if GPIO.input(15) == GPIO.HIGH or GPIO.input(13) == GPIO.HIGH:
-            try:
-                clock_thread.join()
-            except RuntimeError:
-                break
-
-            break
-
-        # RESET button
-        if GPIO.input(11) == GPIO.HIGH:
-            break
 
 
 if __name__ == "__main__":
