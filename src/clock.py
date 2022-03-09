82bb4e41aedc0df9a651a9e3f421a7f0ac64d43a
arthur <arthurtquintao@gmail.com>
2022-03-09 15:56:22 +0100

    remove gpio instructions

--- src/clock.py
+++ src/clock.py
@@ -1,9 +1,5 @@
 import threading
-import RPi.GPIO as GPIO
 from time import strftime
-import lcddriver
-
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # RESET GPIO17
 
 
 def display_clock(lcd, clock_thread=None):
@@ -17,16 +13,3 @@ def display_clock(lcd, clock_thread=None):
                 # Date & Time display
                 lcd.lcd_display_string(strftime('TIME: ' '%H:%M:%S'), 1)
                 lcd.lcd_display_string(strftime('%a, %b %d %Y'), 2)
-
-        # Stop displaying
-        if GPIO.input(15) == GPIO.HIGH or GPIO.input(13) == GPIO.HIGH:
-            try:
-                clock_thread.join()
-            except RuntimeError:
-                break
-
-            break
-
-        # RESET button
-        if GPIO.input(11) == GPIO.HIGH:
-            break
