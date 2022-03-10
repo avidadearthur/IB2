2490e872d8b84a2b9df3c094b26ee5870847c2f1
arthur <arthurtquintao@gmail.com>
2022-03-10 18:22:07 +0100

    change if by while

--- src/buzz.py
+++ src/buzz.py
@@ -18,13 +18,17 @@ seconds_to_beep = 0
 
 while True:
     print(seconds_to_beep)
-    if seconds_to_beep < 1:
+    while seconds_to_beep < 0.5:
         GPIO.output(buzzer, GPIO.HIGH)
-        curr = datetime.now()
-        # get now plus 10 seconds
-        curr_plus_delta = curr + timedelta(seconds=2)
-    elif seconds_to_beep > 1:
+        seconds_to_beep = (curr_plus_delta - datetime.now()).total_seconds()
+
+    curr = datetime.now()
+    # get now plus 10 seconds
+    curr_plus_delta = curr + timedelta(seconds=2)
+
+    while seconds_to_beep > 0.5:
         GPIO.output(buzzer, GPIO.LOW)
+        seconds_to_beep = (curr_plus_delta - datetime.now()).total_seconds()
 
     seconds_to_beep = (curr_plus_delta - datetime.now()).total_seconds()
 
