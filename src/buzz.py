ef95dab5287343bb0330fce2d9562b08feca8665
arthur <arthurtquintao@gmail.com>
2022-03-10 16:13:05 +0100

    Update buzzer code for testing

--- src/buzz.py
+++ src/buzz.py
@@ -13,7 +13,7 @@ GPIO.setup(buzzer, GPIO.OUT)
 while True:
     GPIO.output(buzzer, GPIO.HIGH)
     print("Beep")
-    sleep(0.5)  # Delay in seconds
+    sleep(1)  # Delay in seconds
     GPIO.output(buzzer, GPIO.LOW)
     print("No Beep")
-    sleep(0.5)
+    sleep(1)
