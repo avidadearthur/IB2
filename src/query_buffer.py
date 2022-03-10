697933fc09fb6f4ef382d875e27538a6a74b5dc0
arthur <arthurtquintao@gmail.com>
2022-03-10 15:13:47 +0100

    Update test code

--- src/query_buffer.py
+++ src/query_buffer.py
@@ -6,10 +6,10 @@ seconds_to_new_query = 0
 
 while True:
     # get the date and time for now
-    if seconds_to_new_query == 0:
+    if seconds_to_new_query < 0:
         now = datetime.now()
         # get now plus 10 seconds
-        today_plus_delta = now + timedelta(seconds=10)
+        today_plus_delta = now + timedelta(seconds=3)
 
     # get the seconds from now until Tuesday at midnight
     seconds_to_new_query = (today_plus_delta - datetime.now()).total_seconds()
