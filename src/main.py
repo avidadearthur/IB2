ffcee835943251b6ad18745e19423e649e32067d
arthur <arthurtquintao@gmail.com>
2022-03-10 18:05:07 +0100

    Replace sleep by time counter logic

--- src/main.py
+++ src/main.py
@@ -20,11 +20,22 @@ def set_buzz():
     buzzer = 32
     GPIO.setup(buzzer, GPIO.OUT)
 
+    # Buzzer sleep parameters
+    curr_plus_delta = datetime.now()
+    curr = datetime.now()
+    seconds_to_beep = 0
+
     while True:
-        GPIO.output(buzzer, GPIO.HIGH)
-        sleep(0.5)  # Delay in seconds
-        GPIO.output(buzzer, GPIO.LOW)
-        sleep(0.5)
+
+        if seconds_to_beep < 0:
+            GPIO.output(buzzer, GPIO.HIGH)
+            curr = datetime.now()
+            # get now plus 10 seconds
+            curr_plus_delta = now + timedelta(seconds=0.5)
+        elif seconds_to_beep < -0.5:
+            GPIO.output(buzzer, GPIO.LOW)
+
+        seconds_to_beep = (curr_plus_delta - datetime.now()).total_seconds()
 
         # RESET button
         if GPIO.input(11) == GPIO.HIGH:
