d63379a2cd4b6a21dee986f7a6a5e03fc56c4875
arthur <arthurtquintao@gmail.com>
2022-03-30 14:34:07 +0200

    Add another constrain to call alarm

--- src/main.py
+++ src/main.py
@@ -377,7 +377,7 @@ if __name__ == "__main__":
         time_left = alarm - datetime.now()
 
         if "buzz" not in [th.name for th in threading.enumerate()]:
-            if time_left < timedelta(seconds=0) and not stop_alarm:
+            if time_left < timedelta(seconds=0) and not stop_alarm and len(datetime_alarm) != 0:
                 print("Starting Buzzer thread...")
                 sleep(0.2)
                 buzzer_thread = threading.Thread(target=set_buzz, name="buzz")
